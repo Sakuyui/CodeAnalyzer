66e707deba2a5a0de2b27ec3e604ff9ad621ebfc
Sakuyui39 <405544641@qq.com>
2021-03-14 18:26:44 +0800

    0314

--- src/main/java/com/akb/codeanlyzer/controller/SearchController.java
+++ src/main/java/com/akb/codeanlyzer/controller/SearchController.java
@@ -1,5 +1,6 @@
 package com.akb.codeanlyzer.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import network.Tools;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -7,10 +8,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import se.ast.ASTExtractor;
 
-import java.io.File;
-import java.io.IOException;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -21,7 +25,9 @@ public class SearchController {
     @Autowired
     RedisTemplate redisTemplate;
     @RequestMapping(value = "/search/byFile")
-    public String SearchByFile(@RequestParam("file") MultipartFile file) {
+    @ResponseBody
+    public String SearchByFile(@RequestParam("file") MultipartFile file, @RequestParam(value = "page",defaultValue = "0",required = false)
+                               int page, HttpSession session) {
         String uuid = UUID.randomUUID().toString();
         File f = new File("D:\\storage\\upload\\tmp\\" + uuid  +".java");
         try{
@@ -29,18 +35,34 @@ public class SearchController {
             redisTemplate.opsForValue().set("f_uuid_" + uuid, uuid,300, TimeUnit.SECONDS);
             Map<String, String> param = new HashMap<>();
             param.put("f_uuid", uuid);
+            FileInputStream fis = new FileInputStream(f);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            String code = new String(bis.readAllBytes());
+            String token = ASTExtractor.GetTokenizedSymbolicsNames(code);
+            param.put("tokens", token);
+            param.put("page", "" + page);
             String s = Tools.httpRequest("http://127.0.0.1:5001/feature/byFile",
                     param, false);
-            System.out.println(s);
+            session.setAttribute("s_result", s);
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("status", 200);
+            return jsonObject.toJSONString();
+
         }catch (Exception e){
             if(f.exists()){
                 f.delete();
             }
-            return "/";
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("status", -1);
+            return jsonObject.toJSONString();
         }
 
-        return "/search_byFile";
+
     }
 
 
+    @RequestMapping("/searchResult")
+    public String resultPage(){
+        return "search_byFile";
+    }
 }
